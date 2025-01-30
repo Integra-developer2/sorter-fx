c2d00f98c800412e8a9d1061484bf8c0f4602cc5
teo <developer2.it@integraa.it>
2025-01-30 12:45:26 +0100

    inp

--- src/main/java/app/o3_sorter_stock/ThreadPdf.java
+++ src/main/java/app/o3_sorter_stock/ThreadPdf.java
@@ -1,33 +1,54 @@
 package app.o3_sorter_stock;
 
 public class ThreadPdf extends Thread {
-    private Thread t;
-    private final String threadName;
-    private final String from;
-    private final String to;
-    StepPdf obj;
-    public ThreadPdf( String threadName, StepPdf obj, String from, String to ) {
-        this.threadName = threadName;
-        this.obj = obj;
-        this.from = from;
-        this.to = to;
-    }
-    @Override
-    public void run() {
-        obj.start(from, to);
-    }
-    @Override
-    public void start () {
-        if (t == null) {
-            t = new Thread (this, threadName);
-            t.start ();
-        }
-        ThreadCount.addPdf(this);
-    }
-    public boolean isRunning() {
-        if (t != null) {
-            return t.isAlive();
-        }
-        return false;
-    }
+   private Thread t;
+   private final String threadName;
+   private final ThreadObjPdf obj;
+   public final String from;
+   public final String to;
+   private boolean completedSuccessfully;
+   private Exception exception;
+
+   ThreadPdf(String _threadName, ThreadObjPdf _obj, String _from, String _to) {
+      this.threadName = _threadName;
+      this.obj = _obj;
+      this.from = _from;
+      this.to = _to;
+      this.completedSuccessfully = false;
+      this.exception = null;
+   }
+
+   @Override
+   public void run() {
+      try {
+         this.obj.start(this.from, this.to);
+         this.completedSuccessfully = true;
+      } catch (Exception e) {
+         this.exception = e;
+      }
+   }
+
+   @Override
+   public synchronized void start() {
+      if (this.t == null) {
+         this.t = new Thread(this, this.threadName);
+         this.t.start();
+      }
+   }
+
+   public boolean isRunning() {
+      return this.t != null && this.t.isAlive();
+   }
+
+   public boolean isCompletedSuccessfully() {
+      return this.completedSuccessfully;
+   }
+
+   public boolean hasError() {
+      return this.exception != null;
+   }
+
+   public Exception getException() {
+      return this.exception;
+   }
 }
