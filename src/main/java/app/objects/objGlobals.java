62deec4640fbc392d3a50fdbb4068823c61d5be9
Integra-developer2 <developer2.it@integraa.it>
2025-10-01 12:45:26 +0200

    2.0.3

--- src/main/java/app/objects/objGlobals.java
+++ src/main/java/app/objects/objGlobals.java
@@ -14,7 +14,7 @@ import app.classes.Api;
 import javafx.scene.Scene;
 
 public class objGlobals {
-    public static String version = "SORTER-FX 2.0.2";
+    public static String version = "SORTER-FX 2.0.3";
     public static int totalThreads=1;
     public static String runStep="";
     public static String scanditProgram="it.smaart.sorter.stock";
